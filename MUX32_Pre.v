cc6ae59ad9bc88af457eee4c830458d547197948
Sophia Huang <b07902031@csie.ntu.edu.tw>
2020-12-11 11:21:08 +0800

    change input name of previous mux

--- MUX32_Pre.v
+++ MUX32_Pre.v
@@ -1,17 +1,17 @@
-module MUX32_New(data1_i, data2_i, data3_i, select_i, data_o);
+module MUX32_New(data1_i, data2_i, data3_i, forward_select_i, data_o);
 
 //Ports
 input [31:0] data1_i;
 input [31:0] data2_i;
 input [31:0] data3_i;
-input [1:0] select_i;
+input [1:0] forward_select_i;
 output [31:0] data_o;
 
 //Registers 
 reg [31:0] data_o;
 
-always@(data1_i or data2_i or data3_i or select_i) begin
-	case(select_i)
+always@(data1_i or data2_i or data3_i or forward_select_i) begin
+	case(forward_select_i)
 		2'b00: data_o = data1_i;
 		2'b01: data_o = data2_i;
 		2'b10: data_o = data3_i;
