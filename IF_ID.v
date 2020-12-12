4dbe2bd65a43382e0d86bfa2739ce0d82cd7208d
Sophia Huang <b07902031@csie.ntu.edu.tw>
2020-12-12 15:19:58 +0800

    add IF/ID module

--- IF_ID.v
+++ IF_ID.v
@@ -3,9 +3,8 @@ module IF_ID (clk, PC_i, PC_o, IF_stall, IF_flush, instruction_i, instruction_o)
 //Ports
 input clk;
 input [31:0] PC_i;
-input IF_stall;
-input IF_flush;
 input [31:0] instruction_i;
+input IF_stall, IF_flush;
 output [31:0] PC_o;
 output [31:0] instruction_o;
 
@@ -13,4 +12,14 @@ output [31:0] instruction_o;
 reg [31:0] PC_o;
 reg [31:0] instruction_o;
 
-
+always @(posedge clk) begin
+	if (IFStall == 0 && IFFlush == 0) begin
+		PC_o = PC_i;
+		instruction_o = instruction_i;
+	end
+	else begin
+		PC_o = 32'b0;
+		instruction_o = 32b'0;
+	end
+end
+endmodule
