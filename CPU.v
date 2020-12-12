44c7ab2ad371da1b1484f10b2c3d45613001f5c8
Sophia Huang <b07902031@csie.ntu.edu.tw>
2020-12-12 15:19:08 +0800

    modify CPU.v finish ID stage

--- CPU.v
+++ CPU.v
@@ -10,20 +10,89 @@ input               clk_i;
 input               rst_i;
 input               start_i;
 
-// Wires
+//Wires for IF Stage
+wire [31:0] PCNext_pre, PCNext, PCCurrent, PCBranch;
+wire PCWrite;
+wire PC_signal;
 wire [31:0] Instruction;
-wire [1:0] ALUOp;
-wire [6:0] Opcode;
-wire ALUSrc;
+
+PC_Adder PC_Adder(
+    .data1_in   (PCCurrent),
+    .data2_in   (32'd4),
+    .data_o     (PCNext)
+);
+
+PC_MUX PC_MUX(
+    .branchTaken_i (PC_signal),
+    .addrNotTaken_i (PCNext),
+    .addrTaken_i (PCBranch),
+    .addr_o (PCNext_pre)
+)
+
+PC PC(
+    .clk_i      (clk_i),
+    .rst_i      (rst_i),
+    .start_i    (start_i),
+    .PCWrite_i  (PCWrite),
+    .pc_i       (PCNext_pre),
+    .pc_o       (PCCurrent)
+);
+
+Instruction_Memory Instruction_Memory(
+    .addr_i     (PCCurrent), 
+    .instr_o    (Instruction)
+);
+
+//Wires for IF_ID Stage
+wire [31:0] IF_ID_PC_o, IF_ID_Instruction_o;
+wire IFStall, IFFlush;
+
+IF_ID IF_ID(
+    .clk    (clk_i),
+    .PC_i   (PCCurrent),
+    .PC_o   (IF_ID_PC_o),
+    .IF_stall   (IFStall),
+    .IF_flush   (IFFlush),
+    .instruction_i  (instruction),
+    .Instruction_o  (IF_ID_Instruction_o)
+)
+
+//Wires for ID Stage
+wire [4:0] RegisterReadAddr1, RegisterReadAddr2, RegisterWriteAddr;
 wire RegWrite;
-wire [31:0] PCNext;
-wire [31:0] PCCurrent;
-wire [4:0] RegisterReadAddr1;
-wire [4:0] RegisterReadAddr2;
-wire [4:0] RegisterWriteAddr;
-wire [31:0] RegisterReadData1;
-wire [31:0] RegisterReadData2;
-wire [31:0] RegisterWriteData;
+wire [31:0] RegisterReadData1, RegisterReadData2, RegisterWriteData;
+wire [6:0] Opcode;
+wire [1:0] ALUOp;
+wire NoOp_signal, RegWrite, MemtoReg, MemRead, MemWrite, ALUSrc, Branch;
+
+
+Registers Registers(
+    .clk_i      (clk_i),
+    .RS1addr_i   (RegisterReadAddr1),
+    .RS2addr_i   (RegisterReadAddr2),
+    .RDaddr_i   (RegisterWriteAddr), 
+    .RDdata_i   (ALUResult),
+    .RegWrite_i (RegWrite), 
+    .RS1data_o   (RegisterReadData1), 
+    .RS2data_o   (RegisterReadData2) 
+);
+
+Control Control(
+    .Op_i       (Opcode),
+    .No_Op_i    (NoOp_signal),
+    .RegWrite_o (RegWrite), 
+    .MemReg_o   (MemtoReg), 
+    .MemRead_o  (MemRead), 
+    .MemWrite_o (MemWrite), 
+    .ALUOp_o    (ALUOp),
+    .ALUSrc_o   (ALUSrc),
+    .Branch_o (Branch)
+);
+
+
+/* Below haven't finish yet */
+
+// Wires
 wire [31:0] ReadData2;
 wire [9:0] Function;
 wire [2:0] ALUControl;
@@ -39,44 +108,6 @@ assign RegisterReadAddr2 = Instruction[24:20];
 assign SignExtensionIn = Instruction[31:20];
 assign Function = {Instruction[31:25], Instruction[14:12]};
 assign RegisterWriteAddr = Instruction[11:7];
-
-Control Control(
-    .Op_i       (Opcode),
-    .ALUOp_o    (ALUOp),
-    .ALUSrc_o   (ALUSrc),
-    .RegWrite_o (RegWrite)
-);
-
-
-Adder Add_PC(
-    .data1_in   (PCCurrent),
-    .data2_in   (32'd4),
-    .data_o     (PCNext)
-);
-
-PC PC(
-    .clk_i      (clk_i),
-    .rst_i      (rst_i),
-    .start_i    (start_i),
-    .pc_i       (PCNext),
-    .pc_o       (PCCurrent)
-);
-
-Instruction_Memory Instruction_Memory(
-    .addr_i     (PCCurrent), 
-    .instr_o    (Instruction)
-);
-
-Registers Registers(
-    .clk_i      (clk_i),
-    .RS1addr_i   (RegisterReadAddr1),
-    .RS2addr_i   (RegisterReadAddr2),
-    .RDaddr_i   (RegisterWriteAddr), 
-    .RDdata_i   (ALUResult),
-    .RegWrite_i (RegWrite), 
-    .RS1data_o   (RegisterReadData1), 
-    .RS2data_o   (RegisterReadData2) 
-);
 
 MUX32 MUX_ALUSrc(
     .data1_i    (RegisterReadData2),
