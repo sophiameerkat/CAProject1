8cce21d9ae08a8114b0d10418514ca1f039c7c2c
Sophia Huang <b07902031@csie.ntu.edu.tw>
2020-12-13 18:11:51 +0800

    add CPU.v I am not sure QQ

--- CPU.v
+++ CPU.v
@@ -69,10 +69,10 @@ wire NoOpSignal; //to Control & ID/EX
 wire StallSignal;
 //Control
 assign Opcode = Instruction[6:0];
-wire RegWrite;
-wire MemtoReg;
-wire MemRead;
-wire MemWrite;
+wire RegWrite_ControltoID_EX;
+wire MemtoReg_ControltoID_EX;
+wire MemRead_ControltoID_EX;
+wire MemWrite_ControltoID_EX;
 wire ALUOp;
 wire ALUSrc;
 wire Branch;
@@ -107,10 +107,10 @@ HazardDetectionUnit HazardDetectionUnit(
 
 Control Control(
     .Op_i       (Opcode),
-    .RegWrite_o (RegWrite), 
-    .MemReg_o   (MemtoReg), 
-    .MemRead_o  (MemRead), 
-    .MemWrite_o (MemWrite), 
+    .RegWrite_o (RegWrite_ControltoID_EX), 
+    .MemReg_o   (MemtoReg_ControltoID_EX), 
+    .MemRead_o  (MemRead_ControltoID_EX), 
+    .MemWrite_o (MemWrite_ControltoID_EX), 
     .ALUOp_o    (ALUOp),
     .ALUSrc_o   (ALUSrc),
     .Branch_o (Branch)
@@ -166,10 +166,10 @@ wire [31:0] imm_ID_EXtoMUX;
 ID_EX ID_EX(
     .clk_i  (clk_i),
     //signals
-    RegWrite_i      (RegWrite),
-    MemtoReg_i  (MemtoReg),
-    MemRead_i   (MemRead),
-    MemWrite_i     (MemWrite),
+    RegWrite_i      (RegWrite_ControltoID_EX),
+    MemtoReg_i  (MemtoReg_ControltoID_EX),
+    MemRead_i   (MemRead_ControltoID_EX),
+    MemWrite_i     (MemWrite_ControltoID_EX),
     ALUOp_i     (ALUOp),
     ALUSrc_i    (ALUSrc),
     NoOp_i      (NoOpSignal),
@@ -199,45 +199,125 @@ ID_EX ID_EX(
     imm_o   (imm_ID_EXtoMUX)
 );
 
-
-/* Below haven't finish yet */
-
-
-
-
-
-
-// Wires
-wire [31:0] ReadData2;
-wire [9:0] Function;
+// Wires for EX Stage
+wire [31:0] ALUResult_EX_MEMtoDM;
+wire [31:0] RS1data_MUXtoALU, RS2data_MUXtoALUMUX;
+wire [1:0] Forward_signal_A, Forward_signal_B;
+wire RegWrite_EX_MEMtoMEM_WB;
+wire [4:0] RD_EX_MEMtoMEM_WB;
+wire [31:0] RS2data_ALUMUXtoALU;
 wire [2:0] ALUControl;
-wire [31:0] ALUResult;
+wire [31:0] ALUResult_ALUtoEX_MEM;
 wire ZeroSignal;
 
-assign funct3 = Instruction[14:12];
-assign Function = {Instruction[31:25], Instruction[14:12]};
-assign RegisterWriteAddr = Instruction[11:7];
+MUX32_Pre MUX_data1(
+    .data1_i    (RS1data_ID_EXtoMUX), 
+    .data2_i    (RDdata_MEM_WBtoRegs), 
+    .data3_i    (ALUResult_EX_MEMtoDM),
+    .forward_select_i   (Forward_signal_A), 
+    .data_o     (RS1data_MUXtoALU)
+)
+
+MUX32_Pre MUX_data2(
+    .data1_i    (RS2data_ID_EXtoMUX), 
+    .data2_i    (RDdata_MEM_WBtoRegs), 
+    .data3_i    (ALUResult_EX_MEMtoDM),
+    .forward_select_i   (Forward_signal_B), 
+    .data_o     (RS2data_MUXtoALUMUX)
+)
+
+Forwarding_Unit Forwarding_Unit(
+    .ID_EX_RS1  (RS1_ID_EXtoFU), 
+    .ID_EX_RS2  (RS2_ID_EXtoFU), 
+    .EX_MEM_RegWrite    (RegWrite_EX_MEMtoMEM_WB), 
+    .EX_MEM_Rd  (RD_EX_MEMtoMEM_WB), 
+    .MEM_WB_RegWrite    (RegWrite_MEM_WBtoRegs), 
+    .MEM_WB_Rd  (RDaddr_MEM_WBtoRegs), 
+    .ForwardA   (Forward_signal_A), 
+    .ForwardB   (Forward_signal_B)
+)
 
 MUX32 MUX_ALUSrc(
-    .data1_i    (RegisterReadData2),
+    .data1_i    (RS2data_ID_EXtoMUX),
     .data2_i    (SignExtensionOut),
-    .select_i   (ALUSrc),
-    .data_o     (ReadData2)
-);
-
-  
-ALU ALU(
-    .data1_i    (RegisterReadData1),
-    .data2_i    (ReadData2),
-    .ALUCtrl_i  (ALUControl),
-    .data_o     (ALUResult),
-    .Zero_o     (ZeroSignal)
+    .select_i   (ALUSrc_ID_EXtoMUX),
+    .data_o     (RS2data_ALUMUXtoALU)
 );
 
 ALU_Control ALU_Control(
-    .funct_i    (Function),
-    .ALUOp_i    (ALUOp),
+    .funct_i    (Funct_ID_EXtoALUControl),
+    .ALUOp_i    (ALUOp_ID_EXtoALUControl),
     .ALUCtrl_o  (ALUControl)
+);
+
+ALU ALU(
+    .data1_i    (RS1data_MUXtoALU),
+    .data2_i    (RS2data_ALUMUXtoALU),
+    .ALUCtrl_i  (ALUControl),
+    .data_o     (ALUResult_ALUtoEX_MEM),
+    .Zero_o     (ZeroSignal)
+);
+
+// Wires for EX/MEM Stage
+wire MemtoReg_EX_MEMtoMEM_WB;
+wire MemRead_EX_MEMtoDM;
+wire MemWrite_EX_MEMtoDM;
+wire [31:0] RS2data_EXMEMtoDM;
+
+EX_MEM EX_MEM (
+    .clk_i  (clk_i), 
+    .RegWrite_i     (RegWrite_ID_EXtoEX_MEM), 
+    .MemReg_i   (MemtoReg_ID_EXtoEX_MEM), 
+    .MemRead_i  (MemRead_ID_EXtoEX_MEM), 
+    .MemWrite_i     (MemWrite_ID_EXtoEX_MEM), 
+    .ALUResult_i    (ALUResult_ALUtoEX_MEM), 
+    .RegWrite_o     (RegWrite_EX_MEMtoMEM_WB), 
+    .MemReg_o   (MemtoReg_EX_MEMtoMEM_WB), 
+    .MemRead_o  (MemRead_EX_MEMtoDM), 
+    .MemWrite_o     (MemWrite_EX_MEMtoDM), 
+    .rs2_data_i (RS2data_MUXtoALUMUX), 
+    .rd_addr_i  (RD_ID_EXtoEX_MEM), 
+    .rd_addr_o  (RD_EX_MEMtoMEM_WB), 
+    .ALUResult_o    (ALUResult_EX_MEMtoDM), 
+    .MemData_o  (RS2data_EXMEMtoDM)
+);
+
+//Wires for MEM Stage
+wire [31:0] ReadData_DMtoMEM_WB;
+
+Data_Memory Data_Memory (
+    .clk_i  (clk_i), 
+    .addr_i     (ALUResult_EXMEMtoDM), 
+    .MemRead_i  (MemRead_EX_MEMtoDM),
+    .MemWrite_i     (MemWrite_EX_MEMtoDM),
+    .data_i     (RS2data_EXMEMtoDM),
+    .data_o     (ReadData_DMtoMEM_WB)
+);
+
+//Wires for MEM_WB Stage
+wire [31:0] MemtoReg_MEM_WBtoWBMUX;
+wire [31:0] ALUResult_MEM_WBtoWBMux;
+wire [31:0] ReadData_MEM_WBtoWBMux;
+
+MEM_WB MEM_WB(
+    .clk_i  (clk_i), 
+    .RegWrite_i     (RegWrite_EX_MEMtoMEM_WB), 
+    .MemReg_i   (MemtoReg_ID_EXtoEX_MEM), 
+    .rd_addr_i  (RD_EX_MEMtoMEM_WB), 
+    .RegWrite_o     (RegWrite_MEM_WBtoRegs), 
+    .MemReg_o   (MemtoReg_MEM_WBtoWBMUX), 
+    .data1_i    (ALUResult_EX_MEMtoDM), 
+    .data2_i    (ReadData_DMtoMEMWB), 
+    .data1_o    (ALUResult_MEM_WBtoWBMux), 
+    .data2_o    (ReadData_MEM_WBtoWBMux), 
+    .rd_addr_o  (RDaddr_MEM_WBtoRegs)
+);
+
+MUX32 MUX_WB(
+    .data1_i    (ALUResult_MEM_WBtoWBMux),
+    .data2_i    (ReadData_MEM_WBtoWBMux),
+    .select_i   (MemtoReg_MEM_WBtoWBMUX),
+    .data_o     (RDdata_MEM_WBtoRegs)
 );
 
 endmodule
