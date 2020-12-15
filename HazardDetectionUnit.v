6116b8a806e30de2f73d944e5b841d0b830b7cc2
goodyhsu <b07902043@ntu.edu.tw>
2020-12-15 13:08:17 +0800

    HDU modified

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -27,17 +27,14 @@ assign stallSignal_o = stallSignal_oReg;
 assign PCWriteSignal_o = PCWriteSignal_oReg;
 
 always@(*) begin
-	noOpSignal_oReg <= 0;
-	stallSignal_oReg <= 0;
-	PCWriteSignal_oReg <= 1;
+	noOpSignal_oReg = 0;
+	stallSignal_oReg = 0;
+	PCWriteSignal_oReg = 1;
 	
 	if(MemReadSignal_i && (RD_i != 0) && (RD_i == RS1_i || RD_i == RS2_i)) begin //load, stall
 		noOpSignal_oReg <= 1'b1;
 		stallSignal_oReg <= 1'b1;
 		PCWriteSignal_oReg <= 1'b0;
-	end
-	else begin
-		//do nothing
 	end
 end
 
